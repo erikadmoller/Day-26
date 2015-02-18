5cc95ae0d1e8eaa60c30659195bf6c181d3f427d
Erika Moller <erikamoller@gmail.com>
2015-02-17 22:17:44 -0600

    Working on questions 5 of 5, unable to make working login

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -1,6 +1,29 @@
 angular.module('basic.controllers', []) 
 .controller('loadingCtrl', function($scope, $timeout) {
 
+	// var attempt = 3; // Variable to count number of attempts.
+	// Below function Executes on click of login button.
+	$scope.validate = function(){
+	$scope.username = username.val();
+	$scope.password = password.val();
+	
+	if ( username == "aaron@theironyard.com" && password == "password123"){
+		alert("Login successfully");
+		window.location = "http://www.theironyard.com"; // Redirecting to other page.
+		return false;
+	}
+	// else{
+	// 	attempt --;// Decrementing by one.
+	// 	alert("You have left "+attempt+" attempt;");
+	// 	// Disabling fields after 3 attempts.
+	// if( attempt == 0){
+	// 	document.getElementById("username").disabled = true;
+	// 	document.getElementById("password").disabled = true;
+	// 	document.getElementById("submit").disabled = true;
+	// 	return false;
+	// }
+	// }
+	}
 
 	$scope.numLikes = 0
 	
